ad19ab252f6adebe90d6063c07addd058366e21e
Mr-Moose-dev <filip.lorin@gmail.com>
2023-11-22 23:38:32 +0100

    init

---
+++ src/main.rs
@@ -0,0 +1,16 @@
+use bevy::prelude::*;
+
+pub struct Stryfe;
+
+impl Plugin for Stryfe {
+    fn build(&self, app: &mut App) {
+    }
+}
+
+fn main() {
+    App::new()
+        .add_plugins((DefaultPlugins, Stryfe))
+        .run();
+}
+
+
